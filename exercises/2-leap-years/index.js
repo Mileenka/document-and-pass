049cf52110ed318f4bf19e603f08c82f8e984388
Milena Wasowicz <milena.wasowicz@gmail.com>
2023-07-17 11:29:23 +0200

    initial commit

--- exercises/2-leap-years/index.js
+++ exercises/2-leap-years/index.js
@@ -1,8 +1,14 @@
+'use strict';
+
 /**
+ * Given year is a leap year or not
  *
+ * @param { number } year - The year to check.
+ * @returns { boolean } - True if the year is a leap year, false otherwise.
  */
-const leapYears = function () {
 
-}
-
-module.exports = leapYears
+const leapYears = (year) => {
+    return year % 4 === 0 && (year % 100 !== 0 || year % 400 === 0);
+  };
+  
+module.exports = leapYears;
